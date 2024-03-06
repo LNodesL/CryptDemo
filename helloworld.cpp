64f08a6b6d75d9a89bcac183e07f8380b66a9b40
nodes <159713604+LNodesL@users.noreply.github.com>
2024-03-06 12:57:19 -0500

    Delete helloworld.cpp

--- helloworld.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    MessageBoxA(NULL, "Hello, World!", "Message", MB_OK);
-    return 0;
-}
